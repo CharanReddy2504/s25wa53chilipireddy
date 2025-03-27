6b433b304f5e5bc1a025b0852cc89e45c55b6ad4
CharanReddy2504 <chilipiredd.ycharanreddy@gmail.com>
2025-03-26 22:18:16 -0500

    app.js

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);  // This was correct before, no change needed
 app.use('/mydata', mydataRouter); 
-app.use('/computation', computationRouter); // Correct usage
-
+app.use('/computation', computationRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
